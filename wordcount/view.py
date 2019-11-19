80dddb5fc9a980bb669616fbe775490b16669160
S20180020217 <samruthkumar.k18@iiits.in>
2019-11-19 18:13:00 +0530

    commit5

--- wordcount/view.py
+++ wordcount/view.py
@@ -1,9 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import pyrebase
+Config = {
+    'apiKey': "AIzaSyA6eJc8HWKf-Pn_M0l0LU18t1b44ANRE9g",
+    'authDomain': "eisproject-520c0.firebaseapp.com",
+    'databaseURL': "https://eisproject-520c0.firebaseio.com",
+    'projectId': "eisproject-520c0",
+    'storageBucket': "eisproject-520c0.appspot.com",
+    'messagingSenderId': "38568259347",
+    'appId': "1:38568259347:web:8d6d01c859f83ad4cdf4e6",
+    'measurementId': "G-9CPEN6YSCV"
+  }
+firebase= pyrebase.initialize_app(Config)
+auth=firebase.auth()
 def form(request):
 	return render(request,'formaction.html')
 def count(request):
-	Firstname=request.GET['Firstname']
-	return render(request,'count.html',{'Firstname':Firstname})
+	email=request.GET['email']
+	passWord=requst.Get['password']
+	user=auth.sign_in_with_email_and_password(email,passWord)
+	return render(request,'count.html',{'email':email})
 def about(request):
 	return render(request,'about.html')
